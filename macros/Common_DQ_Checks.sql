9fb80093e5d20a41ee421ac79a8ea92391dc6b15
Rishin1994 <98064293+Rishin1994@users.noreply.github.com>
2022-02-14 16:34:56 +0530

    Delete Common_DQ_Checks.sql
    not required

--- macros/Common_DQ_Checks.sql
+++
@@ -1,8 +0,0 @@
-{% test string_length_check(model, column_name, desired_len) %}
-
-    select *
-    from {{ model }}
-    where {{ column_name }} is not null 
-    and length({{column_name}})<>{{desired_len}}
-
-{% endtest %}
